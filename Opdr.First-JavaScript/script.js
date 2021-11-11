f5c9598c18c68bfb748935e4a72517724b70a6fc
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-11-11 10:57:30 +0100

    Add files via upload

---
+++ Opdr.First-JavaScript/script.js
@@ -0,0 +1,23 @@
+console.log("Hello Winc Academy")
+let name = "Geraisa";
+console.log(name)
+/* Je kan geen variable aanmaken met de naam if*/
+let numb = 4 + 4;
+console.log(numb)
+let numb2 = "4" + "4";
+console.log(numb2)
+/* 2 nummers bij elkaar optellen is mogelijk, maar als je het wilt optellen als een string dan werkt het niet. Dan komt er gewoon 44 te staan*/
+let add = 4 + 4;
+console.log(add)
+let subtraction = 50 - 30;
+console.log(subtraction)
+let multiplication = 30 * 30;
+console.log(multiplication)
+let division = 40 / 12;
+console.log(division)
+let modulus = 20 % 9
+console.log(modulus)
+let age = 36;
+console.log(typeof age)
+let ageOfString = "36";
+console.log(typeof ageOfString)
