d3818877ae1427e10bc2b4d6da87932d866e9e71
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-11-17 11:27:18 +0100

    Add files via upload

---
+++ Opdr.Objects and Arrays/index.js
@@ -0,0 +1,27 @@
+//Deel 1 Objecten:
+
+const person = {
+    name: 'Geraisa',
+    age: 35,
+    evaluations: [7, 10, 9]
+};
+
+console.log(person);
+//console.log(person.name); om alleen de naam te zien
+//console.log(person.age); om alleen de leeftijd te zien
+//console.log(person.evaluations); om alleen de gehaalde cijfers te laten zien.
+
+
+//Deel 2 Arrays:
+
+const array = ["groen", "blauw", "rood"];
+
+array.push("geel");
+array.push(5);
+array.push({greeting: "hi ik ben een object"});
+
+console.log(array[5]);
+//console.log(array.lenth); om de lengte te loggen
+//console.log(array[0]); om de eerste element te laten zien
+//console.log(array[2]); om de laatste element te laten zien
+//console.log(array[5]); om de laatste element te zien
